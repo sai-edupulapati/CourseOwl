0a5f66b4a6e84bf10f66901a22097eb6c79f1d04
sai-edupulapati <89745999+sai-edupulapati@users.noreply.github.com>
2023-06-25 22:35:05 -0400

    completed initial landing page

--- mern/client/course-owl/src/components/landing_page.js
+++ mern/client/course-owl/src/components/landing_page.js
@@ -8,8 +8,10 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import { Paper, createTheme } from '@mui/material';
 import { ThemeProvider } from '@emotion/react';
-import Image from '../assets/home.jpg'
-
+import Image from '../assets/home51.jpg'
+import logo from '../assets/owl.png'
+import Stack from '@mui/material/Stack';
+import Container from '@mui/material/Container';
 
 const font = "'Belanosima', sans-serif"
 const theme = createTheme({
@@ -22,33 +24,59 @@ const theme = createTheme({
 const styles = {
     paperContainer: {
         background: `url(${Image})`,
-        backgroundSize: 'cover'
+        backgroundSize: 'cover',
+        backgroundPosition: 'center'
+    },
+    icon: {
+//      display: 'block',
+      margin: 'auto',
+      width: '35px',
+      top: '30px',
+      left: '0',
+      right: '0',
+//      bottom: '0',
+      position: 'absolute'
     }
 };
 
 export default function ButtonAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
+      <Paper style={styles.paperContainer} sx={{height: "100vh", paddingTop: "15px"}} elevation={0}>
       <AppBar position="static" color='transparent' elevation={0}>
+        <img style={styles.icon} src={logo}></img>
         <Toolbar>
           <IconButton
             size="large"
             edge="start"
             color="inherit"
             aria-label="menu"
-            sx={{ mr: 2 }}
+            sx={{ mr: 2, justifyContent: 'left' }}
           >
             <MenuIcon />
           </IconButton>
           <ThemeProvider theme={theme}>
-              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} align='center' fontSize={25}>
+              <Typography component="div" sx={{ flexGrow: 1 }} fontSize={30} fontWeight={'bold'}>
                 CourseOwl
               </Typography>
               <Button color="inherit">Log In</Button>
           </ThemeProvider>
         </Toolbar>
       </AppBar>
-      <Paper style={styles.paperContainer} sx={{height: "100vh"}}>
+      <Box paddingTop={'10vh'}>
+      <ThemeProvider theme={theme}>
+        <Container maxWidth={'sm'}>
+        <Stack spacing={2}>
+          <Typography component="div" sx={{ flexGrow: 1}} align='center' fontSize={55} fontWeight={'bold'}>
+            Welcome, Students!
+          </Typography>
+          <Typography component="div" sx={{ flexGrow: 1}} align='center' fontSize={25}>
+          Meet CourseOwl, a singular place for you to view professor reviews, build perfect schedules, connect with your peers, and plan your future!
+          </Typography>
+        </Stack>
+        </Container>
+      </ThemeProvider>
+      </Box>
       </Paper>
     </Box>
   );
