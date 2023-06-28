b8c291f17019d53c21a5eadbb7d0f23be2e8d168
kgovil8 <kgovil@purdue.edu>
2023-06-27 23:05:31 -0700

    Prelim Navbar Dropdown Menu

--- mern/client/course-owl/src/components/navbar.js
+++ mern/client/course-owl/src/components/navbar.js
@@ -5,68 +5,86 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import { Paper, createTheme } from '@mui/material';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { Backdrop, createTheme } from '@mui/material';
 import { ThemeContext, ThemeProvider } from '@emotion/react';
-import Image from '../assets/home51.jpg'
-import logo from '../assets/owl.png'
+import Image from '../assets/home51.jpg';
+import logo from '../assets/owl.png';
 import Stack from '@mui/material/Stack';
-import Container from '@mui/material/Container';
-import schedule from '../assets/chat2.png'
-import grades from '../assets/like3.png'
-import chat from '../assets/like4.png'
-import review from '../assets/like2.png'
-import ControlledAccordions from './faq_dropdown';
-import github from '../assets/github.png'
-import linkedin from '../assets/in.png'
 
-const font = "'Belanosima', sans-serif"
+const font = "'Belanosima', sans-serif";
 const theme = createTheme({
-    typography: {
-        fontFamily: font,
-        button: {textTransform: "none"}
-    }
-})
+  typography: {
+    fontFamily: font,
+    button: { textTransform: 'none' },
+  },
+});
 
 const styles = {
-    paperContainer: {
-        background: `url(${Image})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center'
-    },
-    icon: {
-      margin: 'auto',
-      width: '35px',
-      top: '30px',
-      left: '0',
-      right: '0',
-      position: 'absolute'
-    },
-    features: {
-      width: '10vw'
-    }
+  paperContainer: {
+    background: `url(${Image})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  icon: {
+    margin: 'auto',
+    width: '35px',
+    top: '30px',
+    left: '0',
+    right: '0',
+    position: 'absolute',
+  },
+  features: {
+    width: '10vw',
+  },
 };
 
 export default function NavBar() {
-    return (
-        <AppBar position="static" color='transparent' elevation={0}>
-        <img style={styles.icon} src={logo} alt='logo'></img>
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2, justifyContent: 'left' }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <ThemeProvider theme={theme}>
-              <Typography component="div" sx={{ flexGrow: 1 }} fontSize={30} fontWeight={'bold'}>
-                CourseOwl
-              </Typography>
-              <button className='button-54'>Log In</button>
-          </ThemeProvider>
-        </Toolbar>
-      </AppBar>
-    )
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <AppBar position="static" color="transparent" elevation={0}>
+      <img style={styles.icon} src={logo} alt="logo"></img>
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2, justifyContent: 'left' }}
+          onClick={handleClick}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+          MenuListProps={{ 'aria-labelledby': 'basic-button' }}
+        >
+          <MenuItem onClick={handleClose}>Menu Item 1</MenuItem>
+          <MenuItem onClick={handleClose}>Menu Item 2</MenuItem>
+          <MenuItem onClick={handleClose}>Menu Item 3</MenuItem>
+        </Menu>
+        <ThemeProvider theme={theme}>
+          <Typography component="div" sx={{ flexGrow: 1 }} fontSize={30} fontWeight={'bold'}>
+            CourseOwl
+          </Typography>
+          <button className="button-54">Log In</button>
+        </ThemeProvider>
+      </Toolbar>
+      <Backdrop open={Boolean(anchorEl)} sx={{ zIndex: 1 }}>
+        {/* Your blurred background content goes here */}
+      </Backdrop>
+    </AppBar>
+  );
 }
