a825b8faf7ddb7fe9a3fc4374f2551a56452331b
sai-edupulapati <vedupula@purdue.edu>
2023-06-28 21:52:43 -0400

    testing

--- mern/client/course-owl/src/App.js
+++ mern/client/course-owl/src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import ButtonAppBar from './components/landing_page';
 
+// Testing github commit working or not.
+
 function App() {
   return (
     <ButtonAppBar></ButtonAppBar>
