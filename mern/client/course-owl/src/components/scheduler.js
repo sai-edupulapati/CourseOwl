c5683646634ad75d5a166891aa141643527ba88b
kgovil8 <kgovil@purdue.edu>
2023-07-07 00:46:49 -0700

    basic version of dynamic addition of courses to the calendar

--- mern/client/course-owl/src/components/scheduler.js
+++ mern/client/course-owl/src/components/scheduler.js
@@ -1,133 +1,126 @@
 import React, { useState } from 'react';
-import {
-  Typography,
-  Container,
-  Grid,
-  Paper,
-  createTheme,
-  ThemeProvider,
-} from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
-// Set your font and color scheme
-const font = "'Belanosima', sans-serif";
 const theme = createTheme({
   typography: {
-    fontFamily: font,
-    button: { textTransform: 'none' },
-  },
-  palette: {
-    primary: {
-      main: '#YourPrimaryColor',
-    },
-    secondary: {
-      main: '#YourSecondaryColor',
-    },
+    fontFamily: "'Belanosima', sans-serif",
   },
 });
 
 const CalendarPage = () => {
-  const [selectedCourse, setSelectedCourse] = useState(null);
+  const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+  const timeSlots = Array.from({ length: 28 }, (_, index) => {
+    const hour = Math.floor(index / 2) + 6;
+    const minutes = index % 2 === 0 ? '00' : '30';
+    return `${hour}:${minutes}`;
+  });
 
-  // Define your course data with days, timings, and course names
-  const courses = [
-    { day: 'Monday', time: '10:00 AM - 12:00 PM', name: 'Course A' },
-    { day: 'Tuesday', time: '2:00 PM - 4:00 PM', name: 'Course B' },
-    { day: 'Wednesday', time: '9:00 AM - 11:00 AM', name: 'Course C' },
-    // Add more courses here
-  ];
+  const [selectedCourses, setSelectedCourses] = useState([]);
 
-  const handleCourseSelect = (course) => {
-    setSelectedCourse(course);
+  const handleCourseClick = (course) => {
+    setSelectedCourses((prevCourses) => [...prevCourses, course]);
+  };
+
+  const isCourseSelected = (course) => {
+    return selectedCourses.some((selectedCourse) => selectedCourse.name === course.name);
+  };
+
+  const renderCourseOptions = () => {
+    const courseOptions = [
+      { name: 'Course A', day: 'Monday', startTime: '7:00', endTime: '8:00' },
+      { name: 'Course B', day: 'Tuesday', startTime: '9:30', endTime: '11:00' },
+      { name: 'Course C', day: 'Wednesday', startTime: '14:00', endTime: '16:00' },
+      // Add more course options as needed
+    ];
+
+    return courseOptions.map((course) => (
+      <Box
+        key={course.name}
+        onClick={() => handleCourseClick(course)}
+        style={{ cursor: 'pointer' }}
+        bgcolor={isCourseSelected(course) ? '#aaffaa' : 'inherit'}
+        padding="8px"
+        marginBottom="8px"
+      >
+        <Typography variant="body1" align="center" fontWeight="bold">
+          {course.name}
+        </Typography>
+        <Typography variant="body2" align="center">
+          {`${course.startTime} - ${course.endTime}`}
+        </Typography>
+      </Box>
+    ));
+  };
+
+  const renderCalendarItems = () => {
+    return days.map((day) => (
+      <Box
+        key={day}
+        flex="1"
+        border="1px solid #ccc"
+        padding="8px"
+        className="calendar-item"
+        data-day={day}
+      >
+        <Typography variant="h6" align="center" fontWeight="bold">
+          {day}
+        </Typography>
+        {timeSlots.map((time) => {
+          const course = selectedCourses.find(
+            (selectedCourse) => selectedCourse.day === day && selectedCourse.startTime === time
+          );
+          if (course) {
+            const endTimeSlot = timeSlots[timeSlots.indexOf(time) + 1];
+            return (
+              <Box
+                key={`${day}-${time}`}
+                bgcolor="#aaffaa"
+                padding="2px"
+                marginBottom="2px"
+              >
+                <Typography variant="body2" align="center">
+                  {`${time} - ${endTimeSlot}`}
+                </Typography>
+                <Typography variant="body2" align="center" fontWeight="bold">
+                  {course.name}
+                </Typography>
+              </Box>
+            );
+          }
+          return null;
+        })}
+      </Box>
+    ));
   };
 
   return (
     <ThemeProvider theme={theme}>
-      <Container maxWidth="md">
-        <Typography component="h1" variant="h4" align="center">
+      <Box>
+        <Typography variant="h4" align="center" fontWeight="bold">
           Weekly Calendar
         </Typography>
-        <Grid container spacing={2} marginTop={3}>
-          {/* Render the calendar grid */}
-          <Grid item xs={12}>
-            <Paper>
-              {/* Render each day and time slot */}
-              <Grid container>
-                <Grid item xs={2} />
-                <Grid item xs={10} container spacing={2}>
-                  <Grid item xs={2}>
-                    <Typography variant="subtitle1">Time</Typography>
-                  </Grid>
-                  <Grid item xs={2}>
-                    <Typography variant="subtitle1">Monday</Typography>
-                  </Grid>
-                  <Grid item xs={2}>
-                    <Typography variant="subtitle1">Tuesday</Typography>
-                  </Grid>
-                  <Grid item xs={2}>
-                    <Typography variant="subtitle1">Wednesday</Typography>
-                  </Grid>
-                  {/* Add more days here */}
-                </Grid>
-              </Grid>
-            </Paper>
-          </Grid>
-          {/* Render the course selection */}
-          <Grid item xs={12}>
-            <Paper>
-              <Typography component="h2" variant="h6" align="center">
-                Course Selection
+        <Box display="flex">
+          <Box minWidth="100px" padding="8px">
+            <Typography variant="h6" align="center" fontWeight="bold">
+              Time
+            </Typography>
+            {timeSlots.map((time) => (
+              <Typography key={time} variant="body2" align="center">
+                {time}
               </Typography>
-              <Grid container spacing={2} padding={2}>
-                {/* Render each course */}
-                {courses.map((course, index) => (
-                  <Grid item xs={3} key={index}>
-                    <Paper
-                      elevation={selectedCourse === course ? 3 : 0}
-                      onClick={() => handleCourseSelect(course)}
-                      sx={{
-                        padding: '8px',
-                        cursor: 'pointer',
-                        backgroundColor:
-                          selectedCourse === course
-                            ? theme.palette.primary.main
-                            : 'transparent',
-                      }}
-                    >
-                      <Typography
-                        variant="body1"
-                        align="center"
-                        color={
-                          selectedCourse === course
-                            ? 'white'
-                            : 'textPrimary'
-                        }
-                      >
-                        {course.name}
-                      </Typography>
-                    </Paper>
-                  </Grid>
-                ))}
-              </Grid>
-            </Paper>
-          </Grid>
-          {/* Render the selected course */}
-          {selectedCourse && (
-            <Grid item xs={12}>
-              <Paper>
-                <Typography component="h3" variant="subtitle1" align="center">
-                  Selected Course: {selectedCourse.name}
-                </Typography>
-                <Typography variant="body1" align="center">
-                  Day: {selectedCourse.day}
-                </Typography>
-                <Typography variant="body1" align="center">
-                  Time: {selectedCourse.time}
-                </Typography>
-              </Paper>
-            </Grid>
-          )}
-        </Grid>
-      </Container>
+            ))}
+          </Box>
+          {renderCalendarItems()}
+        </Box>
+        <Box>
+          <Typography variant="h6" align="center" fontWeight="bold">
+            Course Options
+          </Typography>
+          {renderCourseOptions()}
+        </Box>
+      </Box>
     </ThemeProvider>
   );
 };
