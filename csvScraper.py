4aee03927111dcdc208b18f5136cf46aedfcd430
Kushagra Govil <89705199+kgovil8@users.noreply.github.com>
2023-07-08 03:14:26 -0700

    Created scraper script for fetching course csv file

---
+++ csvScraper.py
@@ -0,0 +1,82 @@
+import os
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+import shutil
+
+import time
+
+from webdriver_manager.chrome import ChromeDriverManager
+driver = webdriver.Chrome()
+
+# Path to your ChromeDriver executable
+#webdriver_path = '/Users/kushagragovil/Downloads/chromedriver' 
+
+# URL of the webpage with the export button
+url = 'https://timetable.mypurdue.purdue.edu/Timetabling/gwt.jsp?page=classes#name=AAE&term=Fall2023PWL'
+
+# Path to save the downloaded CSV file
+csv_path = '/Users/kushagragovil/Desktop'
+
+# Set up Chrome options
+#chrome_options = webdriver.ChromeOptions()
+#chrome_options.add_argument('--headless')  # Run in headless mode without opening a browser window
+
+# Create a new ChromeDriver service
+#service = Service(webdriver_path)
+
+# Create a new instance of the Chrome driver
+#driver = webdriver.Chrome(service=service, options=chrome_options)
+
+try:
+    # Open the webpage
+    driver.get(url)
+
+    print("hello")
+
+    time.sleep(15)
+
+    # Wait for the "Export" button to be clickable
+    export_button = WebDriverWait(driver, 5).until(
+        EC.element_to_be_clickable((By.XPATH, '/html/body/span[3]/span/span[2]/span[1]/span[3]/span[2]/div/table/tbody/tr[2]/td/div/div[2]/span/button[3]'))
+    )
+
+    print("wefweg")
+
+    # Click the "Export" button
+    export_button.click()
+
+    # Wait for the "Export CSV" button to be clickable
+    export_csv_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '//td[contains(text(), "Export CSV")]'))
+    )
+
+    print("hewe")
+
+    # Click the "Export CSV" button
+    export_csv_button.click()
+
+    time.sleep(15)
+
+    source = '/Users/kushagragovil/Downloads/events.csv'
+    destination = '/Users/kushagragovil/Desktop/eventsaae.csv'
+
+    # Move the file
+    shutil.move(source, destination)
+
+    # Wait for the CSV file to be downloaded
+  
+    #lambda x: csv_path in os.listdir(os.path.dirname(csv_path))
+    
+
+    print("ho")
+
+    print('CSV file downloaded successfully!')
+
+finally:
+    # Quit the browser
+    driver.quit()
