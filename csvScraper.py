b4f9a02443a977c3566391869fbc4bcaec734025
sai-edupulapati <vedupula@purdue.edu>
2023-07-08 06:40:34 -0400

    Update csvScraper.py

--- csvScraper.py
+++ csvScraper.py
@@ -40,8 +40,37 @@ try:
 
     time.sleep(15)
 
+    more_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '/html/body/span[3]/span/span[2]/span[1]/span[3]/span[2]/div/table/tbody/tr[2]/td/div/div[2]/span/button[4]'))
+    )
+
+    more_button.click()
+
+
+    time.sleep(10)
+
+    note_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div/div/table/tbody/tr[2]/td'))
+    )
+
+    note_button.click()
+
+    time.sleep(10)
+
+    more_button.click()
+
+    time.sleep(5)
+
+    enrol_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div/div/table/tbody/tr[8]/td'))
+    )
+
+    enrol_button.click()
+    
+    time.sleep(10)
+
     # Wait for the "Export" button to be clickable
-    export_button = WebDriverWait(driver, 5).until(
+    export_button = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.XPATH, '/html/body/span[3]/span/span[2]/span[1]/span[3]/span[2]/div/table/tbody/tr[2]/td/div/div[2]/span/button[3]'))
     )
 
@@ -79,4 +108,4 @@ try:
 
 finally:
     # Quit the browser
-    driver.quit()
+    driver.quit()
