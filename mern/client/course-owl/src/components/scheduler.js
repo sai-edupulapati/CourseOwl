d90b6c7f432e4e9905badd420accde9dc65bc3a4
kgovil8 <kgovil@purdue.edu>
2023-07-08 00:30:42 -0700

    scheduler ui functionality completed

--- mern/client/course-owl/src/components/scheduler.js
+++ mern/client/course-owl/src/components/scheduler.js
@@ -8,6 +8,13 @@ const styles = {
   },
   main: {
     flexGrow: "1"
+  },
+  eventList: {
+    width: "200px",
+    padding: "10px"
+  },
+  eventButton: {
+    marginBottom: "5px"
   }
 };
 
@@ -41,44 +48,80 @@ const Calendar = () => {
     headerDateFormat: "dddd" // Set the format to display the day of the week
   });
 
+  const [eventButtons, setEventButtons] = useState([]);
+  const [eventsVisible, setEventsVisible] = useState(false); // State to track if events are visible
+
   useEffect(() => {
-    const events = [
+    const initialEvents = [
       {
         id: 1,
         text: "Event 1",
-        start: "2023-10-02T10:30:00",
-        end: "2023-10-02T13:00:00"
+        start: ["2023-10-02T11:00:00", "2023-10-04T11:00:00"],
+        end: ["2023-10-02T13:00:00", "2023-10-04T13:00:00"]
       },
       {
         id: 2,
         text: "Event 2",
-        start: "2023-10-03T09:30:00",
-        end: "2023-10-03T11:30:00",
+        start: ["2023-10-03T09:30:00"],
+        end: ["2023-10-03T11:30:00"],
         backColor: "#6aa84f"
       },
       {
         id: 3,
         text: "Event 3",
-        start: "2023-10-03T12:00:00",
-        end: "2023-10-03T15:00:00",
+        start: ["2023-10-03T12:00:00"],
+        end: ["2023-10-03T15:00:00"],
         backColor: "#f1c232"
       },
       {
         id: 4,
         text: "Event 4",
-        start: "2023-10-01T11:30:00",
-        end: "2023-10-01T14:30:00",
+        start: ["2023-10-01T11:30:00"],
+        end: ["2023-10-01T14:30:00"],
         backColor: "#cc4125"
       },
     ];
 
     const startDate = "2023-10-02";
 
-    calendarRef.current.control.update({ startDate, events });
+    calendarRef.current.control.update({ startDate });
+
+    // Create event buttons
+    const buttons = initialEvents.map(event => (
+      <button
+        key={event.id}
+        style={styles.eventButton}
+        onClick={() => handleEventButtonClick(event)}
+      >
+        {event.text}
+      </button>
+    ));
+
+    setEventButtons(buttons);
   }, []);
+
+  const handleEventButtonClick = event => {
+    const dp = calendarRef.current.control;
+    for (let i = 0; i < event.start.length; i++) {
+      dp.events.add({
+        start: event.start[i],
+        end: event.end[i],
+        id: DayPilot.guid(),
+        text: event.text
+      });
+    }
+  };
+
+  const handleShowEvents = () => {
+    setEventsVisible(true);
+  };
 
   return (
     <div style={styles.wrap}>
+      <div style={styles.eventList}>
+        <button onClick={handleShowEvents}>Show Events</button>
+        {eventsVisible && eventButtons}
+      </div>
       <div style={styles.main}>
         <DayPilotCalendar
           {...calendarConfig}
