375bc58913497baaae0279302bd2915c8745422f
sai-edupulapati <vedupula@purdue.edu>
2023-07-20 02:35:32 -0400

    Created basic frontend for chat. still need to connect to mongo

--- mern/client/course-owl/src/components/chat_room.js
+++ mern/client/course-owl/src/components/chat_room.js
@@ -2,19 +2,12 @@ import { useEffect } from "react";
 import { useApp } from "./RealmApp";
 import NavBar from "./navbar";
 import { useNavigate } from "react-router-dom";
+import "./chat_styles.css"
+import { Box, Stack, TextField } from "@mui/material";
 
 export default function Chat() {
     const app = useApp()
     const navigate = useNavigate()
-
-    // useEffect(() => {
-    //     if (app.currentUser) {
-    //       console.log(app.currentUser.id)
-    //       setButtonText("Log Out")
-    //     } else {
-    //       setButtonText("Log In")
-    //     }
-    //   }, [app.currentUser])
 
     useEffect(() => {
         if (!app.currentUser) {
@@ -23,8 +16,14 @@ export default function Chat() {
     }, [app.currentUser])
 
     return (
-        <div>
+        <Box sx={{ flexGrow: 1 }} bgcolor={'rgb(219, 227, 236)'} minHeight={'100vh'}>
             <NavBar></NavBar>
-        </div>
+            <Stack direction={'row'}>
+                <Box>
+                    <TextField></TextField>
+                </Box>
+                <Box></Box>
+            </Stack>
+        </Box>
     )
 }
