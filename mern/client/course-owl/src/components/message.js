40680a44e19e8319a91113affe6fe244b4b082b7
sai-edupulapati <vedupula@purdue.edu>
2023-07-20 22:58:16 -0700

    minor changes

--- mern/client/course-owl/src/components/message.js
+++ mern/client/course-owl/src/components/message.js
@@ -1,14 +1,26 @@
 import { Stack } from '@mui/material';
 import logo from '../assets/owl.png';
+import { createTheme } from '@mui/material';
 import "./chat_styles.css"
+import { ThemeProvider } from '@emotion/react';
+
+const font = "'Belanosima', sans-serif"
+const theme = createTheme({
+    typography: {
+        fontFamily: font,
+        button: {textTransform: "none"}
+    }
+})
 
 export default function Message() {
     return (
         <div className="message">
             <div className="messageTop">
                 <Stack direction={'row'} alignItems={'center'}>
-                    <img src={logo} className='messageImage'></img>
-                    <p className="messageText">Hello this is a message</p>
+                    <ThemeProvider theme={theme}>
+                        <img src={logo} className='messageImage'></img>
+                        <p className="messageText">Hello this is a message</p>
+                    </ThemeProvider>
                 </Stack>
             </div>
             <div className="messageBottom">1 hour ago</div>
